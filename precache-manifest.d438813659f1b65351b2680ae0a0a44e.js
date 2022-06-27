6a152780391e6ff64623eb9d95c35b81ca820f88
Ayguid <guidoaimar@gmail.com>
2022-06-27 14:29:52 -0300

    first commit

---
+++ precache-manifest.d438813659f1b65351b2680ae0a0a44e.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33fd3cbaafb13f833fe8",
+    "url": "/daptee-front/css/app.5329d00f.css"
+  },
+  {
+    "revision": "b11f2105408c88e95789",
+    "url": "/daptee-front/css/chunk-03f86607.80a741ed.css"
+  },
+  {
+    "revision": "55f482b95d98ce36129f",
+    "url": "/daptee-front/css/chunk-12612e4e.4555f00f.css"
+  },
+  {
+    "revision": "318e7eb9666fdcd4834e",
+    "url": "/daptee-front/css/chunk-c6438cb2.6be038a5.css"
+  },
+  {
+    "revision": "e4afae8edcd3e9b0e278",
+    "url": "/daptee-front/css/chunk-vendors.ac7a6bf6.css"
+  },
+  {
+    "revision": "223a61dd7ecf6007efa9",
+    "url": "/daptee-front/css/products.f0222215.css"
+  },
+  {
+    "revision": "2bae01860e8daf5d1aba",
+    "url": "/daptee-front/css/users.d75cb1a5.css"
+  },
+  {
+    "revision": "d6a6f8878adb0d8e69f9fa2e0b622924",
+    "url": "/daptee-front/fonts/Roboto-Black.d6a6f887.ttf"
+  },
+  {
+    "revision": "c3332e3b8feff748ecb0c6cb75d65eae",
+    "url": "/daptee-front/fonts/Roboto-BlackItalic.c3332e3b.ttf"
+  },
+  {
+    "revision": "b8e42971dec8d49207a8c8e2b919a6ac",
+    "url": "/daptee-front/fonts/Roboto-Bold.b8e42971.ttf"
+  },
+  {
+    "revision": "cebd892d1acfcc455f5e52d4104f2719",
+    "url": "/daptee-front/fonts/Roboto-Italic.cebd892d.ttf"
+  },
+  {
+    "revision": "881e150ab929e26d1f812c4342c15a7c",
+    "url": "/daptee-front/fonts/Roboto-Light.881e150a.ttf"
+  },
+  {
+    "revision": "68ea4734cf86bd544650aee05137d7bb",
+    "url": "/daptee-front/fonts/Roboto-Medium.68ea4734.ttf"
+  },
+  {
+    "revision": "c16d19c2c0fd1278390a82fc245f4923",
+    "url": "/daptee-front/fonts/Roboto-MediumItalic.c16d19c2.ttf"
+  },
+  {
+    "revision": "8a36205bd9b83e03af0591a004bc97f4",
+    "url": "/daptee-front/fonts/Roboto-Regular.8a36205b.ttf"
+  },
+  {
+    "revision": "66209ae01f484e46679622dd607fcbc5",
+    "url": "/daptee-front/fonts/Roboto-Thin.66209ae0.ttf"
+  },
+  {
+    "revision": "7bcadd0675fe47d69c2d8aaef683416f",
+    "url": "/daptee-front/fonts/Roboto-ThinItalic.7bcadd06.ttf"
+  },
+  {
+    "revision": "8d2a39592ea748d7f9bd25067aa6a0d6",
+    "url": "/daptee-front/img/Logo-Daptee.8d2a3959.svg"
+  },
+  {
+    "revision": "0935c0b9fa3cbee9c6f3c58df10048e8",
+    "url": "/daptee-front/index.html"
+  },
+  {
+    "revision": "33fd3cbaafb13f833fe8",
+    "url": "/daptee-front/js/app.846ae718.js"
+  },
+  {
+    "revision": "b11f2105408c88e95789",
+    "url": "/daptee-front/js/chunk-03f86607.64da7d89.js"
+  },
+  {
+    "revision": "55f482b95d98ce36129f",
+    "url": "/daptee-front/js/chunk-12612e4e.d9d367ad.js"
+  },
+  {
+    "revision": "a4407c504352eb7cb4ff",
+    "url": "/daptee-front/js/chunk-2d0b66f9.e734ac7d.js"
+  },
+  {
+    "revision": "ce851607a1d9190d0e2e",
+    "url": "/daptee-front/js/chunk-3c51fc94.75523460.js"
+  },
+  {
+    "revision": "318e7eb9666fdcd4834e",
+    "url": "/daptee-front/js/chunk-c6438cb2.4f315ea6.js"
+  },
+  {
+    "revision": "e4afae8edcd3e9b0e278",
+    "url": "/daptee-front/js/chunk-vendors.2df0f846.js"
+  },
+  {
+    "revision": "223a61dd7ecf6007efa9",
+    "url": "/daptee-front/js/products.cacb5f74.js"
+  },
+  {
+    "revision": "2bae01860e8daf5d1aba",
+    "url": "/daptee-front/js/users.64fa3b84.js"
+  },
+  {
+    "revision": "88ae7fd77f64552a88c11484589513c6",
+    "url": "/daptee-front/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/daptee-front/robots.txt"
+  }
+]);
